0e53505e7bc872a14668dd60153d8b0b2d93a4a7
Tap <46947072+ttappr@users.noreply.github.com>
2024-01-22 02:21:03 -0800

    Error type changes.

--- doc/src-files.js
+++ doc/src-files.js
@@ -1,4 +1,4 @@
 var srcIndex = JSON.parse('{\
-"hexchat_api":["",[],["callback_data.rs","consts.rs","context.rs","hexchat.rs","hexchat_callbacks.rs","hexchat_entry_points.rs","hook.rs","lib.rs","list_item.rs","list_iterator.rs","plugin.rs","thread_facilities.rs","threadsafe_context.rs","threadsafe_hexchat.rs","threadsafe_list_iterator.rs","user_data.rs","utils.rs"]]\
+"hexchat_api":["",[],["callback_data.rs","consts.rs","context.rs","errors.rs","hexchat.rs","hexchat_callbacks.rs","hexchat_entry_points.rs","hook.rs","lib.rs","list_item.rs","list_iterator.rs","plugin.rs","thread_facilities.rs","threadsafe_context.rs","threadsafe_hexchat.rs","threadsafe_list_iterator.rs","user_data.rs","utils.rs"]]\
 }');
 createSrcSidebar();
