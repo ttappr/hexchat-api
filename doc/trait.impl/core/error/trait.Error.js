b9285c0606ec06ef94855400a78def61c7cc8bcc
Tap <46947072+ttappr@users.noreply.github.com>
2025-03-31 14:08:11 -0700

    Updating for v0.3.3

--- doc/trait.impl/core/error/trait.Error.js
+++ doc/trait.impl/core/error/trait.Error.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"hexchat_api":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"hexchat_api/enum.HexchatError.html\" title=\"enum hexchat_api::HexchatError\">HexchatError</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["hexchat_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"hexchat_api/enum.HexchatError.html\" title=\"enum hexchat_api::HexchatError\">HexchatError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[284]}
--- doc/trait.impl/core/ops/index/trait.Index.js
+++ doc/trait.impl/core/ops/index/trait.Index.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"hexchat_api":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.75.0/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.75.0/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"hexchat_api/struct.ListItem.html\" title=\"struct hexchat_api::ListItem\">ListItem</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["hexchat_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/index/trait.Index.html\" title=\"trait core::ops::index::Index\">Index</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.85.1/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"hexchat_api/struct.ListItem.html\" title=\"struct hexchat_api::ListItem\">ListItem</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[395]}
