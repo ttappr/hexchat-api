91e528cc6cc6e428bd5037a204a02bd9c1177982
Tap <46947072+ttappr@users.noreply.github.com>
2025-04-01 23:05:03 -0700

    `From<...> for time_t` changed to `From<...> for i64` to avoid 32-bit platform compilation issues.

--- src/threadsafe_list_iterator.rs
+++ src/threadsafe_list_iterator.rs
@@ -312,7 +312,7 @@ impl From<ThreadSafeFieldValue> for u64 {
     }
 }
 
-impl From<ThreadSafeFieldValue> for time_t {
+impl From<ThreadSafeFieldValue> for i64 {
     fn from(v: ThreadSafeFieldValue) -> Self {
         v.time()
     }
