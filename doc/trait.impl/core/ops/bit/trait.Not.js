648a383bce9ee2eea25bc55da4091aef23917d94
Tap <46947072+ttappr@users.noreply.github.com>
2025-04-02 23:50:18 -0700

    Updating for v0.3.6

---
+++ doc/trait.impl/core/ops/bit/trait.Not.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["hexchat_api",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"enum\" href=\"hexchat_api/enum.ChanFlag.html\" title=\"enum hexchat_api::ChanFlag\">ChanFlag</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"enum\" href=\"hexchat_api/enum.FD.html\" title=\"enum hexchat_api::FD\">FD</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"enum\" href=\"hexchat_api/enum.IgnFlag.html\" title=\"enum hexchat_api::IgnFlag\">IgnFlag</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[758]}
---
+++ doc/trait.impl/enumflags2/_internal/trait.RawBitFlags.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["hexchat_api",[["impl RawBitFlags for <a class=\"enum\" href=\"hexchat_api/enum.ChanFlag.html\" title=\"enum hexchat_api::ChanFlag\">ChanFlag</a>"],["impl RawBitFlags for <a class=\"enum\" href=\"hexchat_api/enum.FD.html\" title=\"enum hexchat_api::FD\">FD</a>"],["impl RawBitFlags for <a class=\"enum\" href=\"hexchat_api/enum.IgnFlag.html\" title=\"enum hexchat_api::IgnFlag\">IgnFlag</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[395]}
